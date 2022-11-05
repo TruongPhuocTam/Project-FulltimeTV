829ec01ae0e771e45f6e2950ea3e67373eca0088
TruongPhuocTam <latv11011@gmail.com>
2022-11-05 17:01:13 +0700

    Tamcoder

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -1,12 +1,38 @@
-"use strict";
+'use strict'
 
-var app = angular.module('myapp', []);
-app.controller('myctrl', function ($scope, $http) {
-  $scope.Listmatch = [];
-  $http.get("./Pulic/Data/data.json").then(function (res) {
-    $scope.Listmatch = res.data;
-    console.log($scope.Listmatch);
-  }, function (res) {
-    alert('Error');
-  });
-});
+var app = angular.module('myapp', [])
+app.controller('myctrl', ($scope, $http) => {
+  // Lấy dữ liệu trận đấu
+  $scope.Listmatch = []
+  $http.get('../../Public/Data/datamatch.json').then(
+    res => {
+      $scope.Listmatch = res.data
+      console.log($scope.Listmatch)
+    },
+    function (res) {
+      alert('Error Loading DataMatch')
+    }
+  )
+  // Lấy dữ liệu link youtube
+  $scope.Listlink = []
+  $http.get('../../Public/Data/datalinkyoutube.json').then(
+    res => {
+      $scope.Listlink = res.data
+      console.log($scope.Listlink)
+    },
+    function (res) {
+      alert('Error Loading DataLink')
+    }
+  )
+  // Lấy dữ liệu link website bóng đá
+  $scope.Listlinkweb = []
+  $http.get('../../Public/Data/datalinkweb.json').then(
+    res => {
+      $scope.Listlinkweb = res.data
+      console.log($scope.Listlinkweb)
+    },
+    function (res) {
+      alert('Error Loading DataLinkWeb')
+    }
+  )
+})
