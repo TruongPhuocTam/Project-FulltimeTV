b69d92eeeeef5aa8b80a1bba057fdfe5d4a6181d
TruongPhuocTam <latv11011@gmail.com>
2022-11-05 19:31:33 +0700

    Update main.dev.js

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -4,7 +4,7 @@ var app = angular.module('myapp', [])
 app.controller('myctrl', ($scope, $http) => {
   // Lấy dữ liệu trận đấu
   $scope.Listmatch = []
-  $http.get('../../Public/Data/datamatch.json').then(
+  $http.get('./Public/Data/datamatch.json').then(
     res => {
       $scope.Listmatch = res.data
       console.log($scope.Listmatch)
@@ -15,7 +15,7 @@ app.controller('myctrl', ($scope, $http) => {
   )
   // Lấy dữ liệu link youtube
   $scope.Listlink = []
-  $http.get('../../Public/Data/datalinkyoutube.json').then(
+  $http.get('./Public/Data/datalinkyoutube.json').then(
     res => {
       $scope.Listlink = res.data
       console.log($scope.Listlink)
@@ -26,7 +26,7 @@ app.controller('myctrl', ($scope, $http) => {
   )
   // Lấy dữ liệu link website bóng đá
   $scope.Listlinkweb = []
-  $http.get('../../Public/Data/datalinkweb.json').then(
+  $http.get('./Public/Data/datalinkweb.json').then(
     res => {
       $scope.Listlinkweb = res.data
       console.log($scope.Listlinkweb)
