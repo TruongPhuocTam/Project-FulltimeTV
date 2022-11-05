a57f46709e5cc9bb1148edce1ba7b85a80001c97
TruongPhuocTam <latv11011@gmail.com>
2022-11-05 19:55:21 +0700

    tamcoder

--- Public/AngularJS/dist/main.dev.js
+++ Public/AngularJS/dist/main.dev.js
@@ -4,7 +4,7 @@ var app = angular.module('myapp', []);
 app.controller('myctrl', function ($scope, $http) {
   // Lấy dữ liệu trận đấu
   $scope.Listmatch = [];
-  $http.get("../Public/Data/datamatch.json").then(function (res) {
+  $http.get("./Public/Data/datamatch.json").then(function (res) {
     $scope.Listmatch = res.data;
     console.log($scope.Listmatch);
   }, function (res) {
@@ -12,7 +12,7 @@ app.controller('myctrl', function ($scope, $http) {
   }); // Lấy dữ liệu link youtube
 
   $scope.Listlink = [];
-  $http.get("../Public/Data/datalinkyoutube.json").then(function (res) {
+  $http.get("./Public/Data/datalinkyoutube.json").then(function (res) {
     $scope.Listlink = res.data;
     console.log($scope.Listlink);
   }, function (res) {
@@ -20,7 +20,7 @@ app.controller('myctrl', function ($scope, $http) {
   }); // Lấy dữ liệu link website bóng đá
 
   $scope.Listlinkweb = [];
-  $http.get("../Public/Data/datalinkweb.json").then(function (res) {
+  $http.get("./Public/Data/datalinkweb.json").then(function (res) {
     $scope.Listlinkweb = res.data;
     console.log($scope.Listlinkweb);
   }, function (res) {
